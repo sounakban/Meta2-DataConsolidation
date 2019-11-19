d000483b59c1032831e98978101fb88f424bf8ff
Sounak Banerjee <sounakban.science@gmail.com>
2019-11-19 17:50:02 -0500

    Fetch schema

--- Fetch Schema/getStructure.py
+++ Fetch Schema/getStructure.py
@@ -2,9 +2,6 @@ import pandas as pd
 import re
 from io import StringIO
 
-# info=pd.read_csv('../input/UserInfo.tsv',delimiter='\t',encoding='utf-8')
-# print(list(info.columns.values)) #file header
-
 file_list = open("../Fetch Files/MetaTwo_fileList.txt")
 fileCount=0
 header_list=[]
@@ -13,29 +10,23 @@ for dataFile in file_list:
         continue
     dataFile = dataFile.strip()     # Remove spaces from begining or end of file name
     
+    # Gets the number of lines before the header containing meta-data
     skip_lines = 0
     with open(dataFile) as temp:
         while temp.readline().count('\t') < 4:
             skip_lines+=1
-        # print(temp.readline(), skip_lines)
 
-    # with open(dataFile) as temp:
-    #     lines = temp.readlines()
-    #     print(lines[20:21])
-
+    # Replaces extra tabs in the file for pandas suitable format
     with open(dataFile) as temp:
         lines = temp.read()
         lines = re.sub('\t\t+','',lines)
     
     print(dataFile)
-    content=pd.read_csv(StringIO(lines), delimiter='\t', skiprows=skip_lines)
-    # content=pd.read_csv(dataFile, delimiter='\t', encoding='utf-8', skiprows=skip_lines-1)
+    content=pd.read_csv(StringIO(lines), delimiter='\t', encoding='utf-8', skiprows=skip_lines)
     # print(list(content.columns.values)) #file header
-    if not set(content.columns.values) in header_list:
+    if not set(content.columns.values) in header_list:      # Create a list of header combinations
         header_list.append(set(content.columns.values))
     fileCount+=1
-    # if fileCount == 5:
-    #     break
 
 with open('header_list.txt', 'w') as f:
     for header in header_list:
