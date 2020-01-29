3020733af4c346449001ad8903116c3579812309
Sounak Banerjee <sounakban.science@gmail.com>
2020-01-29 15:19:17 -0500

    minor changes

--- 5. Get SQL/utilities.py
+++ 5. Get SQL/utilities.py
@@ -5,10 +5,10 @@ columns_data = ['gameID', 'ts', 'system_ticks', 'event_type', 'episode_number', 
                 'level', 'score', 'lines_cleared', 'evt_id', 'evt_data1', \
                 'evt_data2', 'curr_zoid', 'next_zoid', 'board_rep', 'zoid_rep']
 columns_metadata = ["gameID", "Meta-Two build", "Exp. Start Time", "Game Start", "GameStart Tick", \
-            "CPU Tick Frequency", "SID", "USID", "ECID", "Environment", "Task", \
-            "SessionNr.", "GameNr.", "Input type", "Connected Inputs", "randSeed", \
-            "Screen resolution", "Screen dpi", "Fullscreen", "Window height", \
-            "Window width", "md5sum study", "md5sum task", "md5sum environment"]
+                    "CPU Tick Frequency", "SID", "USID", "ECID", "Environment", "Task", \
+                    "SessionNr.", "GameNr.", "Input type", "Connected Inputs", "randSeed", \
+                    "Screen resolution", "Screen dpi", "Fullscreen", "Window height", \
+                    "Window width", "md5sum study", "md5sum task", "md5sum environment"]
 
 
 def get_schema():
