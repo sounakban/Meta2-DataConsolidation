158c45a7a00a81fd39a15ec71a0ca5ea4db288f5
Sounak Banerjee <sounakban.science@gmail.com>
2020-03-23 22:54:24 -0400

    updated CSV

--- 5. Get SQL/generateCSV.py
+++ 5. Get SQL/generateCSV.py
@@ -1,15 +1,7 @@
 import pandas as pd
 import re
 from io import StringIO
-
-columns_data = ['gameID', 'ts', 'system_ticks', 'event_type', 'episode_number', \
-                'level', 'score', 'lines_cleared', 'evt_id', 'evt_data1', \
-                'evt_data2', 'curr_zoid', 'next_zoid', 'board_rep', 'zoid_rep']
-columns_metadata = ["gameID", "Meta-Two build", "Exp. Start Time", "Game Start", "GameStart Tick", \
-            "CPU Tick Frequency", "SID", "USID", "ECID", "Environment", "Task", \
-            "SessionNr.", "GameNr.", "Input type", "Connected Inputs", "randSeed", \
-            "Screen resolution", "Screen dpi", "Fullscreen", "Window height", \
-            "Window width", "md5sum study", "md5sum task", "md5sum environment"]
+from utilities import get_schema, get_SQLConnection, get_metaDataColumns, get_dataColumns
 
 
 def grab_data(file_content, gameID):
@@ -35,7 +27,7 @@ def grab_data(file_content, gameID):
     extra_columns = updated_content[1].count("\t") - updated_content[0].count("\t")
     temp = pd.read_csv(StringIO(updated_content), delimiter='\t', encoding='utf-8')
     temp.insert(0, "gameID", [gameID]*temp.shape[0], True)
-    temp = temp[columns_data]
+    temp = temp[get_dataColumns()]
     
     return temp
 
@@ -61,11 +53,13 @@ def change_format(df):
     if 'GameType' in df.columns:
         new_df = df
         new_df.rename(columns={'GameType':'Environment', 'GameTask':'Task', 'Session':'SessionNr.'}, inplace=True)
-        new_df.insert(len(new_df), "md5sum study", [""], True)
-        new_df.insert(len(new_df), "md5sum task", [""], True)
-        new_df.insert(len(new_df), "md5sum environment", [""], True)
-        new_df.insert(6, "USID", new_df['SID'], True)
-        new_df.insert(13, "Connected Inputs", [""], True)
+        new_df.insert(len(new_df.columns), "md5sum study", [""], True)
+        new_df.insert(len(new_df.columns), "md5sum task", [""], True)
+        new_df.insert(len(new_df.columns), "md5sum environment", [""], True)
+        if not "USID" in new_df.columns:
+            new_df.insert(6, "USID", new_df['SID'], True)
+        if not "Connected Inputs" in new_df.columns:
+            new_df.insert(13, "Connected Inputs", [""], True)
         return new_df
     else:
         return df
@@ -73,20 +67,22 @@ def change_format(df):
 
 def merge_dataFrames(main_df, new_df, type):
     if type == "meta":
-        change_format(new_df)
+        new_df = change_format(new_df)
     main_df = pd.concat([main_df, new_df], sort=False)
     return main_df
 
 
-def execute(gameID):
+def execute(gameID, begin, end):
+    gameID+=1
+    data, metaData = get_schema()
+
     file_list = open("../1. Fetch Files/MetaTwo_fileList.txt")
     fileCount=0
 
-    data = pd.DataFrame(columns=columns_data)
-    metaData = pd.DataFrame(columns=columns_metadata)
-
     for dataFile in file_list:
         fileCount+=1
+        if fileCount < begin:
+            continue
         print(fileCount)
         if "eye" in dataFile:       # Skip if data is eye data
             fileCount-=1
@@ -97,50 +93,42 @@ def execute(gameID):
         # Replace extra tabs in the file for pandas suitable format
         with open(dataFile) as temp:
             content = temp.readlines()
-            # print(content[0:25])
 
         # Donot change order of the following function calls 'grab_data' modifies 'content'
         new_metaData = grab_metadata(content, gameID)
         new_data = grab_data(content, gameID)
 
-        # new_metaData.to_csv(r'./File2.csv')
-        # new_data.to_csv(r'./File.csv')
-
         data = merge_dataFrames(data, new_data, "data")
         metaData = merge_dataFrames(metaData, new_metaData, "meta")
 
-        gameID+=1
-
         # To avoid running out of emory write current data and clear out old data
-        if fileCount%5 == 0:
+        if fileCount%30 == 0:
             writemode = 'a'
             header = False
-            if  fileCount == 5:
+            if  fileCount == 30:
                 writemode = 'w'
                 header = True
-            metaData.to_csv(r'./File2.csv', index=False, mode=writemode, header=header)
+            metaData.to_csv(r'./metaFile.csv', index=False, mode=writemode, header=header)
             data.to_csv(r'./File.csv', index=False, mode=writemode, header=header)
-            data = pd.DataFrame(columns=columns_data)
-            metaData = pd.DataFrame(columns=columns_metadata)
+            data, metaData = get_schema()
 
-        if fileCount == 10:
+        if fileCount == end:
             break
 
-        # break
-
-    if  fileCount < 5:
+    if  fileCount < 30:
         writemode = 'w'
         header = True
     else:
         writemode = 'a'
         header = False
-    metaData.to_csv(r'./File2.csv', index=False, mode=writemode, header=header)
+    metaData.to_csv(r'./metaFile.csv', index=False, mode=writemode, header=header)
     data.to_csv(r'./File.csv', index=False, mode=writemode, header=header)
 
     # print("Total number of files parsed : ", fileCount)
 
 
-
-
-# The parameter to the execute function is the number to start the primary key for each game
-execute(1)
+for i in range(0, 1300, 100):
+    begin = i
+    end = begin+30
+    gameID = 0
+    execute(gameID, begin, end)
